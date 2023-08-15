5b5ce9fa8c7cbcab4b915ce1a533edd5054e0bb5
Botir <btrghstk@gmail.com>
2023-08-15 05:01:31 +0300

    commit

--- url-shortening-api-landing-page/netlify/functions/shortenLink.js
+++ url-shortening-api-landing-page/netlify/functions/shortenLink.js
@@ -1,24 +1,24 @@
-const fetch = require('node-fetch');
-
 const handler = async (event, context) => {
-  const { url } = JSON.parse(event.body);
-
-  const API_ENDPOINT = `https://api.shrtco.de/v2/shorten?url=${url}`;
-
-  try {
-    const response = await fetch(API_ENDPOINT);
-    const data = await response.json();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
-};
-
-module.exports = { handler };
+    const fetch = await import('node-fetch').then(module => module.default);
+    const { url } = JSON.parse(event.body);
+  
+    const API_ENDPOINT = `https://api.shrtco.de/v2/shorten?url=${url}`;
+  
+    try {
+      const response = await fetch(API_ENDPOINT);
+      const data = await response.json();
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: error.message }),
+      };
+    }
+  };
+  
+  module.exports = { handler };
+  
