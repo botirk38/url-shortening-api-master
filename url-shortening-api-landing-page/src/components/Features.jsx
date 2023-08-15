ca43055698f94b41f43fd4f98707fb1772378e93
Botir <btrghstk@gmail.com>
2023-08-15 12:39:44 +0300

    commit styling changes

--- url-shortening-api-landing-page/src/components/Features.jsx
+++ url-shortening-api-landing-page/src/components/Features.jsx
@@ -11,7 +11,7 @@ function Features(){
             </div>
 
             
-            <div className='relative grid grid-cols-1 z-50 gap-[8rem] lg:gap-0 grid-rows-auto place-items-center text-center w-full sm:gap-10 h-auto bg-[#F2F2F2]  sm:grid-cols-3 sm:grid-rows-1'>
+            <div className='relative grid grid-cols-1 z-50 gap-[8rem] lg:gap-4 xl:gap-0 grid-rows-auto place-items-center text-center w-full sm:gap-10 h-auto bg-[#F2F2F2]  sm:grid-cols-3 sm:grid-rows-1'>
                 
                 <div className=" rounded-md relative flex flex-col sm:h-auto justify-center w-full items-center max-w-xs sm:max-w-sm p-3 sm:p-0 sm:px-3 lg:items-start lg:justify-start lg:text-start lg:after:left-full bg-white after:absolute after:h-60 after:w-2 after:top-60 after:-z-50 after:left-40 after:bg-primary-cyan sm:after:rotate-90 sm:after:top-10   ">
                     <picture className=" bg-primary-dark-violet p-6 absolute bottom-60 rounded-full sm:bottom-80 lg:right-70 lg:bottom-[13rem]">
